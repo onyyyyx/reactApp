f90a7115655b9eb2570185977ff861b5c4f948b1
onyyyyx <onyyyyx@proton.me>
2024-11-10 19:35:19 +0100

    Minor update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ const DATA = [
   {id: "todo-0", name: "Eat", completed: true},
   {id: "todo-1", name: "Sleep", completed: false},
   {id: "todo-2", name: "Repeat", completed: false},
-  {id: "todo-3", name: "Learn React", completed: false}
 ]
 
 createRoot(document.getElementById("root")).render(
